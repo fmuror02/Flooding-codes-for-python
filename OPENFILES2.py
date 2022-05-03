3b1e14aaf6e6fd33093b4306946919f65dbec452
fmuror02 <96955976+fmuror02@users.noreply.github.com>
2022-05-03 16:43:15 +0200

    Add files via upload

---
+++ OPENFILES2.py
@@ -0,0 +1,1048 @@
+n = 0
+txt = ('#####'*10000)
+
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
+f = open(f'file{n}.txt','w')
+f.write(txt)
+f.close()
+n = n + 1
+print(n)
